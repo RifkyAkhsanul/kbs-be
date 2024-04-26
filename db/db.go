a566c7c5f380e514fb465e615c17937135f0960b
RifkyHadi7 <rifkyhadi7@gmail.com>
2024-04-27 05:05:07 +0700

    db

--- db/db.go
+++ db/db.go
@@ -4,18 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/microsoft/go-mssqldb"
 )
 
 var server = "kbs-data.database.windows.net"
 var port = 1433
+var password = "Cahgreedy79."
 var user = "kbs-data"
 var database = "kbs-data"
 
 func NewDriver() (*sql.DB, error) {
-	password := os.Getenv("PASSWORD")
 
 	con := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;", server, user, password, port, database)
 	db, err := sql.Open("sqlserver", con)
