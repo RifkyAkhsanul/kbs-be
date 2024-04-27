b378e7431dba070474dea94243d22e6f6a1380da
RifkyHadi7 <rifkyhadi7@gmail.com>
2024-04-27 16:56:15 +0700

    getinfoformID json

--- controllers/info.controllers.go
+++ controllers/info.controllers.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"github.com/RifkyAkhsanul/kbs-be/models"
 	"github.com/labstack/echo/v4"
 )
+
+type GetInfoRequest struct {
+	ID int `json:"id"`
+}
 
 func GetInfo(c echo.Context) error {
 	result, err := models.GetInfo()
@@ -19,10 +24,17 @@ func GetInfo(c echo.Context) error {
 }
 
 func GetInfofromID(c echo.Context) error {
-	id := c.FormValue("id")
-	idInt, err := strconv.Atoi(id)
+	var req GetInfoRequest
+	decoder := json.NewDecoder(c.Request().Body)
+	err := decoder.Decode(&req)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Invalid request body: " + err.Error()})
+	}
+
+	id := req.ID
+	idInt, err := strconv.Atoi(strconv.Itoa(id)) // Convert to int for consistency
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Error converting ID to integer: " + err.Error()})
 	}
 
 	result, err := models.GetInfofromID(idInt)
